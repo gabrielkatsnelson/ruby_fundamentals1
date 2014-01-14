db888f384ccc3b76249349bddd62e8190bea6238
gabrielkatsnelson <gbrl.ka@gmail.com>
2014-01-14 12:58:23 -0500

    second commit

---
+++ exercise2.rb
@@ -0,0 +1,27 @@
+# puts 'Hello World'
+# puts "Gabriel"
+# name = gets.chomp
+# puts "Your name is #{name}"
+# a = 20 
+# b = 65
+# puts "The value of a is #{a}." # The value of a is 20. 
+# puts "The value of a is #{b}." # The value of b is 65. 
+# puts "The value of a plus b is #{a + b}." # The value of a plus b is 85. 
+# puts "Hello\t\tworld"
+# puts "Hello\b\b\b\b\b\bGoodbye world"
+# puts "Hello\rStart over world"
+# puts "1. Hello\n2. World"
+
+# For a 20% tip
+puts 55* 0.20
+
+#adding an integer to a string
+puts 55+ '1'.to_i
+
+#multiplication
+a=45628
+b=7839
+puts "The result of multiplying #{a} with #{b} is #{a*b}"
+
+#boolean
+puts (true && false) || (false && true) || !(false && false)
