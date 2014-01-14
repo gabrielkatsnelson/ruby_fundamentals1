222f7818cf7cec1a2c25037e328699c6322e0f73
gabrielkatsnelson <gbrl.ka@gmail.com>
2014-01-14 13:54:26 -0500

    IM SO HAPPY I GOT THIS

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 	puts "What is your name?" 
 		name = gets.chomp 
 			puts "Hi #{name}!"
+
+
+	puts "How old are you?"
+		age = gets.chomp.to_i
+		year = 2014 
+			puts "You must have been born around #{year - age}!"
+
