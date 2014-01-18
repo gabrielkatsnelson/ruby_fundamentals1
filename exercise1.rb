121c0294833fe4f6a9a9bee7ef17f4c14534f898
gabrielkatsnelson <gbrl.ka@gmail.com>
2014-01-18 17:20:25 -0500

    final commit

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,14 @@
-puts 2
-puts 3
-puts 2 != 3
+# puts "Enter Amount"
+# amount = gets.chomp
+# puts "Enter %"
+# pct = gets.chomp
+# puts "#{amount.to_f*pct.to_f/100}"
+
+# # puts 55+ '1'.to_f
+# )
+
+# puts (true && false) || (false && true) || !(false && false
+
+# xyz = gets.strip
+# puts "word #{xyz} end"
+
